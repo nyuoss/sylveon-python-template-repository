5868747e44d69f75e47ec40bd9e3556278e08426
calvinatian <48587962+calvinatian@users.noreply.github.com>
2024-02-07 19:21:01 -0500

    Add testing

--- src/sylveon/__main__.py
+++ src/sylveon/__main__.py
@@ -2,6 +2,13 @@
 # recommended not to use "if __name__ == __main__" pattern
 # when importing from package.__main__, __name__ will include the package name
 # but importing from __main__ has a different meaning
-print("Hello World")
+
+
+def hello_world():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    print(hello_world())
 
 # change this to do component stuff
